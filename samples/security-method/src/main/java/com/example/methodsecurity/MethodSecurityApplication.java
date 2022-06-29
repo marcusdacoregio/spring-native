0c96a5ef43e5eb54b9bafec95992349f418da637
Marcus Da Coregio <marcusdacoregio@gmail.com>
2022-06-29 09:27:01 -0300

    AOT Hints

--- samples/security-method/src/main/java/com/example/methodsecurity/MethodSecurityApplication.java
+++ samples/security-method/src/main/java/com/example/methodsecurity/MethodSecurityApplication.java
@@ -1,13 +1,35 @@
 package com.example.methodsecurity;
 
+import org.thymeleaf.spring6.expression.SpringStandardExpressionObjectFactory;
+import org.thymeleaf.spring6.view.ThymeleafView;
+
+import org.springframework.aot.hint.MemberCategory;
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import org.springframework.aot.hint.TypeReference;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportRuntimeHints;
 
 @SpringBootApplication
+@ImportRuntimeHints(MethodSecurityApplication.ThymeleafRuntimeHints.class)
 public class MethodSecurityApplication {
 
 	public static void main(String[] args) throws Throwable {
 		SpringApplication.run(MethodSecurityApplication.class, args);
+	}
+
+	static class ThymeleafRuntimeHints implements RuntimeHintsRegistrar {
+
+		@Override
+		public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+			hints.reflection().registerType(ThymeleafView.class, builder -> builder.withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS));
+			hints.reflection().registerType(SpringStandardExpressionObjectFactory.class, builder -> builder.withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS));
+			hints.reflection().registerType(TypeReference.of("org.thymeleaf.spring6.expression.Mvc$Spring41MvcUriComponentsBuilderDelegate"), builder -> builder
+					.withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS));
+			hints.resources().registerPattern("*.html");
+		}
+
 	}
 
 }
--- samples/security-method/src/main/java/com/example/methodsecurity/WebSecurityConfig.java
+++ samples/security-method/src/main/java/com/example/methodsecurity/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.methodsecurity;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,7 +12,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableMethodSecurity
 public class WebSecurityConfig {
     @Bean
     public UserDetailsService userDetailsService() {
